eec1296f8264073e7f2e8370df60f123b861979f
chrinore <chrinoren@gmail.com>
2025-11-02 23:52:56 +0100

    user fonction

--- backend/src/repositories/user.repository.js
+++ backend/src/repositories/user.repository.js
@@ -1,14 +1,22 @@
 const prisma = require("../config/prismaClient");
 
 class UserRepository {
-
   async create(userData) {
     return await prisma.users.create({
       data: userData,
     });
   }
 
+  async findAll() {
+    return await prisma.users.findMany({
+      orderBy: {
+        created_at: "desc",
+      },
+    });
+  }
+
   async findByEmail(email) {
+    // if (!email) return null;
     return await prisma.users.findUnique({
       where: { email },
     });
--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -3,32 +3,23 @@ const argon2 = require("argon2");
 
 class UserService {
   async createUser(userData) {
-    // Vérifier si l'email existe déjà
     const existingUser = await userRepository.findByEmail(userData.email);
     if (existingUser) {
       throw new Error("Un utilisateur avec cet email existe déjà");
     }
 
-    // Valider l'email
     if (!this.isValidEmail(userData.email)) {
       throw new Error("Format d'email invalide");
     }
 
-    // Valider l'âge
-    if (!this.isValidAge(userData.birthdate)) {
-      throw new Error("L'utilisateur doit avoir au moins 13 ans");
-    }
-
-    // Hasher le password ICI ✅
     const hashedPassword = await argon2.hash(userData.password, {
       type: argon2.argon2id,
-      memoryCost: 2 ** 12,
-      timeCost: 2,
+      memoryCost: 2 ** 15,
+      timeCost: 3,
       hashLength: 50,
-      parallelism: 3,
+      parallelism: 2,
     });
 
-    // Créer l'utilisateur
     const newUser = await userRepository.create({
       name: userData.name,
       first_name: userData.first_name,
@@ -45,22 +36,6 @@ class UserService {
   isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  }
-
-  isValidAge(birthdate) {
-    const birth = new Date(birthdate);
-    const today = new Date();
-    const age = today.getFullYear() - birth.getFullYear();
-    const monthDiff = today.getMonth() - birth.getMonth();
-
-    if (
-      monthDiff < 0 ||
-      (monthDiff === 0 && today.getDate() < birth.getDate())
-    ) {
-      return age - 1 >= 13;
-    }
-
-    return age >= 13;
   }
 }
 
