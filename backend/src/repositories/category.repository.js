c299a9c270494ff724cf26938c2664eca02d67ca
chrinore <chrinoren@gmail.com>
2025-11-06 23:29:24 +0100

    ajout category feature

--- backend/src/repositories/category.repository.js
+++ backend/src/repositories/category.repository.js
@@ -14,9 +14,16 @@ class categoryRepository {
   async getAllCategories() {
     return await prisma.category.findMany();
     // return getAll;
-    }
-    
-    
+  }
+  async getAllCategoriesByname(name) {
+    return await prisma.category.findUnique({ where: { name: name } });
+  }
+  async updateCategory(name, categoryUpdateData) {
+    return await prisma.category.update({
+      where: { name: name },
+      data: categoryUpdateData,
+    });
+  }
 }
 
 module.exports = new categoryRepository();
