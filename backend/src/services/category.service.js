f0c6cf2b9c16b1a78602723694f1f3169273d059
chrinore <chrinoren@gmail.com>
2025-11-06 23:51:45 +0100

    ajout category feature

--- backend/src/services/category.service.js
+++ backend/src/services/category.service.js
@@ -1,7 +1,18 @@
 const categoryRepository = require("../repositories/category.repository.js");
 
 class categoryServices {
-  // hello
+  async createCategory(categoryData) {
+    const existingCategorie = await categoryRepository.getAllCategoriesByname(
+      categoryData.name
+    );
+    if (existingUser) {
+      throw new Error("une categorie similaire existe déjà");
+    }
+    const createCatagory = await categoryRepository.createCategory({
+      name: categoryData.name,
+      description: categoryData.description,
+    });
+  }
 }
 
 module.exports = new categoryServices();
