c4566ccd16fb03c09dc7968b8e72ba534a08c44c
chrinore <chrinoren@gmail.com>
2025-11-21 12:24:49 +0100

    ajout products feature change attribute

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -2,35 +2,38 @@ const productService = require("../services/product.service");
 
 class productController {
   async createProduct(req, res) {
-    const { reference, name, description, price, state, categoryName, file } =
-      req.body;
-  if (
-    !reference || !name || !description || !price || !state || !categoryName || !file
-  ) {
-    return res
-      .status(404)
-      .json({ message: "Veuillez remplir les champs vide !" });
-  }
-  try {
-    const createProduct = await productService.createProduct({
-      reference, name, description, price, state, categoryName, file
-    });
-  } catch (error) {
-    if (error.message.includes("existe déjà")) {
-      return res.status(409).json({
-        message: " Un produit avec ce nom existe déjà",
+    const { name, description, price, state, categoryName, file } = req.body;
+    if (!name || !description || !price || !state || !categoryName || !file) {
+      return res
+        .status(404)
+        .json({ message: "Veuillez remplir les champs vide !" });
+    }
+    try {
+      const createProduct = await productService.createProduct({
+        reference,
+        name,
+        description,
+        price,
+        state,
+        categoryName,
+        file,
+      });
+    } catch (error) {
+      if (error.message.includes("existe déjà")) {
+        return res.status(409).json({
+          message: " Un produit avec ce nom existe déjà",
+        });
+      }
+      if (error.message.includes("n'existe pas")) {
+        return res.status(409).json({
+          message: "La catégorie spécifiée n'existe pas",
+        });
+      }
+      return res.status(500).json({
+        success: false,
+        message: "Erreur lors de la création du produit",
+        error: error.message,
       });
     }
-    if (error.message.includes("n'existe pas")) {
-      return res.status(409).json({
-        message: "La catégorie spécifiée n'existe pas",
-      });
-    }
-    return res.status(500).json({
-      success: false,
-      message: "Erreur lors de la création du produit",
-      error: error.message,
-    });
-  }
   }
 }
